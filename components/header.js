d4e3795ca4bfe78b84befe97c16164e8ef307f2f
Centumjoonho <ghwnsgkgk@kakao.com>
2023-01-11 15:42:19 +0900

    2023-01-11_v1_deploy

--- components/header.js
+++ components/header.js
@@ -15,11 +15,12 @@ export default function Header() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                className="w-10 h-10 p-2 text-white bg-red-500"
+                className="w-10 h-10 p-2 bg-inherit text-sky-600 dark:text-white"
                 viewBox="0 0 24 24"
               >
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
+
               <span className="ml-3 text-xl">Centum 준호</span>
             </a>
           </Link>
